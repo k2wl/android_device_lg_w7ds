966fc8b2578d24a153dee260a8186fc07e43be76
k2wl <kar2wlan@gmail.com>
2014-09-03 07:17:16 +0530

    vanir update

--- cm.mk
+++ cm.mk
@@ -1,12 +1,22 @@
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Inherit device configuration
+$(call inherit-product, device/lge/w7/full_w7.mk)
+
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
 # Release name
 PRODUCT_RELEASE_NAME := LG L90
-PRODUCT_NAME := cm_w7
 
-$(call inherit-product, device/lge/w7/full_w7.mk)
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := hammerhead
+PRODUCT_NAME := cm_w7
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := w7
+PRODUCT_MANUFACTURER := LGE
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
